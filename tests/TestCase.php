4554ecf237f7b0a4dde121e11eae872c40fb9169
Ashraf Amir <ashrafahmadamir@gmail.com>
2025-07-30 11:40:26 +0800

    refactor: Remove unused Passport import from AuthenticationApiTest and clean up TestCase setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,14 +10,14 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        
+
         // Use Passport's testing helpers - point to the correct key location
         Passport::loadKeysFrom(__DIR__.'/../storage');
-        
+
         // Set up Passport client after database is refreshed
         $this->setUpPassport();
     }
-    
+
     protected function setUpPassport(): void
     {
         $this->artisan('passport:client', [
